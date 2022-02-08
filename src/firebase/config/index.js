4308f628bef79720f1677aa4dfb70043ec09e7d5
Rima <rimameih@gmail.com>
2022-02-08 21:24:36 +0700

    hide api key

--- src/firebase/config/index.js
+++ src/firebase/config/index.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAXzj2tN7h4mxzehhgr1R1D9Gz0SfHjtCg",
-  authDomain: "clone-fbd62.firebaseapp.com",
-  projectId: "clone-fbd62",
-  storageBucket: "clone-fbd62.appspot.com",
-  messagingSenderId: "775063092750",
-  appId: "1:775063092750:web:b3ceac3f016acccde1065f",
-  measurementId: "G-ZYTSLYLS84",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
